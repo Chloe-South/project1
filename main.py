1523557acdb2e36262075c7dacc9e46284ef45a0
ChanHeeBae <ch0329b@gmail.com>
2023-06-08 17:16:59 +0900

    제곱근 추가

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def sub_func(n1, n2):
 def mul_func(n1, n2):
     return n1 * n2
 
+def root_func(n1, n2):
+    return n1 ** n2
 
 #전역 변수부
 num1, num2, res = 100, 200, 0
@@ -23,3 +25,5 @@ print(num1, '-', num2, '=', res)
 res = mul_func(num1, num2)
 print(num1, '*', num2, '=', res)
 
+res = root_func(num1, num2)
+print(num1, '**', num2, '=', res)
